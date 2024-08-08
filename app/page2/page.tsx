d031cd3ce0125737e7fd6c36155cefac684f4bee
Kauã Matheus <kmsilva3@minha.fag.edu.br>
2024-08-07 21:38:42 -0300

    Criação de uma nova página de teste!!

---
+++ app/page2/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Página 2</h1>
+    </main>
+  );
+}
